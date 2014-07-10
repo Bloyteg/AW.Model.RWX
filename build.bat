7ccf98b3136808055a9662df9add9b5ce3b75d85
Mr. Byte <bytemr@gmail.com>
2014-07-09 19:19:29 -0600

    Add nuget package restore step.

--- build.bat
+++ build.bat
@@ -9,6 +9,8 @@ if not "%PackageVersion%" == "" (
    set version=-Version %PackageVersion%
 )
 
+cmd /c %nuget% restore
+
 REM Build
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild AW.Model.RWX.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
 
